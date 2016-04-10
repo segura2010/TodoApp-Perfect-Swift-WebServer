138d7710b2242c599703c1158f0054b32cd8aceb
alberto__segura <alberto.segura.delgado@gmail.com>
2016-04-10 13:23:12 +0200

    Initial Commit

---
+++ PerfectHello/PerfectHello.h
@@ -0,0 +1,19 @@
+//
+//  PerfectHello.h
+//  PerfectHello
+//
+//  Created by Alberto on 10/4/16.
+//  Copyright © 2016 Alberto. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PerfectHello.
+FOUNDATION_EXPORT double PerfectHelloVersionNumber;
+
+//! Project version string for PerfectHello.
+FOUNDATION_EXPORT const unsigned char PerfectHelloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PerfectHello/PublicHeader.h>
+
+
